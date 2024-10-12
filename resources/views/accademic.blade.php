24a0228bc0e366de71bbe85a13202aebe0a6193f
Raymond Machary <raymond.machary@gmail.com>
2024-10-12 17:12:17 +0300

    version 5

--- resources/views/accademic.blade.php
+++ resources/views/accademic.blade.php
@@ -20,11 +20,21 @@
 <section class="py-5 bg-light" id="scroll-target">
     <div class="container px-5 my-5">
         <div class="row gx-5 align-items-center">
-            <div class="col-lg-6"><img class="img-fluid rounded mb-5 mb-lg-0" src="https://dummyimage.com/600x400/343a40/6c757d" alt="..." /></div>
+            <div class="col-lg-6"><img class="img-fluid rounded mb-5 mb-lg-0" src="images/performance2023.jpeg" alt="..." /></div>
             <div class="col-lg-6">
-                <h2 class="fw-bolder">Our founding</h2>
-                <p class="lead fw-normal text-muted mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto est, ut esse a labore aliquam beatae expedita. Blanditiis impedit numquam libero molestiae et fugit cupiditate, quibusdam expedita, maiores eaque quisquam.</p>
-            </div>
+                <h2 class="fw-bolder">Performance of National Examination - 2023</h2>
+                <p class="lead fw-normal text-muted mb-0">In 2023, Bethsaida Secondary School witnessed an outstanding academic performance, with an impressive 93% of students being selected to join Advanced Secondary Schools. This remarkable achievement represents a significant leap from previous years, showcasing the dedication and hard work of both students and teachers.
+
+                    To put this in perspective, 2021 saw 59 students (59%) advance to higher education, followed by a surge in 2022 with 87 students making the transition. However, 2023 truly stands out, with 26 out of 29 students (90%) being selected due to their excellent performance, while only a small 7% were not selected.
+
+                    This upward trajectory highlights the potential within our school, but it also underscores the need for continued support. With additional resources, we can ensure that this success becomes the norm for all students, regardless of financial barriers. Your support will help sustain and elevate this momentum, empowering more young minds to achieve their dreams and contribute positively to their communities.
+
+
+
+
+
+
+                     </div>
         </div>
     </div>
 </section>
@@ -32,14 +42,27 @@
 <section class="py-5">
     <div class="container px-5 my-5">
         <div class="row gx-5 align-items-center">
-            <div class="col-lg-6 order-first order-lg-last"><img class="img-fluid rounded mb-5 mb-lg-0" src="https://dummyimage.com/600x400/343a40/6c757d" alt="..." /></div>
+            <div class="col-lg-6 order-first order-lg-last"><img class="img-fluid rounded mb-5 mb-lg-0" src="images/performance2022.jpeg" alt="..." /></div>
             <div class="col-lg-6">
-                <h2 class="fw-bolder">Growth &amp; beyond</h2>
-                <p class="lead fw-normal text-muted mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto est, ut esse a labore aliquam beatae expedita. Blanditiis impedit numquam libero molestiae et fugit cupiditate, quibusdam expedita, maiores eaque quisquam.</p>
-            </div>
+                <h2 class="fw-bolder">Performance of National Examination - 2022</h2>
+                <p class="lead fw-normal text-muted mb-0">Bethsaida Secondary School has shown remarkable improvement in academic performance between 2021 and 2022. In 2021, 59 students, representing 59%, advanced to higher education and joined Advanced Secondary Schools. By 2022, this number surged significantly, with 87 students successfully passing and securing placements—an impressive increase of 47%.
+
+                    This upward trend reflects the dedication of both students and teachers, as well as the growing support of our donors. However, while we celebrate this achievement, many students still face financial challenges that hinder their educational progress. With your continued support, we can ensure that every deserving student has the opportunity to pursue higher education and break the cycle of poverty through knowledge. Together, we can build on this progress and make even greater strides in the years to come. </div>
         </div>
     </div>
-{{-- </section>
+ </section>
+<!-- About section one-->
+<section class="py-5 bg-light" id="scroll-target">
+    <div class="container px-5 my-5">
+        <div class="row gx-5 align-items-center">
+            <div class="col-lg-6"><img class="img-fluid rounded mb-5 mb-lg-0" src="images/performance2021.jpeg" alt="..." /></div>
+            <div class="col-lg-6">
+                <h2 class="fw-bolder">Performance of National Examamination - 2021</h2>
+                <p class="lead fw-normal text-muted mb-0">In 2021, out of all Form Four students at Bethsaida Secondary School, 59% successfully advanced to higher education, securing placements in Advanced Secondary Schools. This achievement reflects the hard work and perseverance of our students, despite the many challenges they face. However, 41% of students were either not selected or face uncertainties, largely due to financial constraints. With your support, we can increase the number of students who continue their education, helping them reach their full potential and create brighter futures for themselves and their communities. </div>
+        </div>
+    </div>
+</section>
+{{--
 <!-- Team members section-->
 <section class="py-5 bg-light">
     <div class="container px-5 my-5">
@@ -78,7 +101,7 @@
             </div>
         </div> --}}
     {{-- </div> --}}
-</section>
+{{-- </section> --}}
 </main>
 
 @endsection
--- resources/views/publication.blade.php
+++ resources/views/publication.blade.php
@@ -1,4 +1,4 @@
-
+d
 @include('layout.layouts')
 @section('machary')
 
@@ -6,6 +6,9 @@
 
 <div class="container p-4">
     <div class="row g-5">
+        <div class="col-md-3">
+            <a href="{{asset('pdf/newslatter_bethsaida.pdf')}}" target="_blank"><img src="{{asset('images/Bethsaida_Newsletter.png')}}" alt="Newsletter September 2024" width="100"></a>
+        </div>
         <div class="col-md-9">
 
         <article class="blog-post">
@@ -15,19 +18,47 @@
           <p>Welcome to our new newsletter, Which higlite the success story of our orphanage secondary School. On this newsletter you will be able to see great news about stories and success story of the bethsaida orphange school projects</p>
           <hr>
         </article>
+         </div>
+
+
+
+
+        <div class="col-md-3">
+            <a href="{{asset('pdf/Bethsaida orphage education Centre_Rais.pdf')}}" target="_blank"><img src="{{asset('images/Report20241.png')}}" alt="Newsletter September 2024" width="100"></a>
+        </div>
+        <div class="col-md-9">
+
+        <article class="blog-post">
+          <h6 class="display-6 link-body-emphasis mb-1">Report of fiancial Aid 2024 -Kiswahili</h6>
+          <p class="blog-post-meta">September 1, 2024 by <a href="#">Machary</a></p>
+
+          <p>Welcome to our new financial proposal. This document highlights key areas where we seek collaboration with the community, organizations, and other institutions to support our orphanage center's mission. Inside, you'll find detailed insights into our ongoing activities and the vital needs we aim to address together. We believe that through your partnership, we can create a lasting impact on the lives of the children we serve, and together, we can build a brighter future for them. We invite you to explore these opportunities and join us in making a difference.   <hr>
+        </article>
+
+        </div>
+
+
+        <!----another news   ---->
+
+        <div class="col-md-3">
+            <a href="{{asset('pdf/childact.pdf')}}" target="_blank"><img src="{{asset('images/image.png')}}" alt="Newsletter September 2024" width="100"></a>
+        </div>
+        <div class="col-md-9">
+
+        <article class="blog-post">
+          <h6 class="display-6 link-body-emphasis mb-1"> The school is operate under  child home licence Regulation,2011  </h6>
+          <p class="blog-post-meta">September 1, 2024 by <a href="#">Machary</a></p>
+
+          <p>Bethsaida Secondary School operates under the umbrella of the Bethsaida Orphanage Education Centre, which is registered in accordance with the Child Act of 2011. This framework not only emphasizes our commitment to providing quality education but also ensures that we uphold the rights and welfare of the children in our care. By adhering to these regulations, we strive to create a safe and nurturing environment that supports the academic and personal growth of every student. Your support can help us continue this vital work and expand our reach to provide even more opportunities for the children in our community. </article>
+
+        </div>
+
+
+
     </div>
-
-    <div class="col-md-3">
-        <a href="{{asset('pdf/newslatter_bethsaida.pdf')}}" target="_blank"><img src="{{asset('images/Bethsaida_Newsletter.png')}}" alt="Newsletter September 2024" width="50%"></a>
-    </div>
-
-
-      </div>
-    </div>
-
 </div>
 
-</body>
+
 @endsection
 
 @include('layout.layour2')
